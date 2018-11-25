df4f4483c88a2167d2ff87db788cbc4f27767840
philbates85 <phillipmbates85@gmail.com>
2018-11-25 14:58:44 -0800

    updated python password checker

--- TutorialPrograms/password_checker.py
+++ TutorialPrograms/password_checker.py
@@ -6,9 +6,12 @@ while x < 3:
 print("Password Checker Program")
 
 correct_password = "python123"
+name = input("Enter your name: ")
+lastName = input("Enter your last name: ")
 password = input("Enter password: ")
 
 while correct_password != password:
     password = input("Wrong password! Enter Again: ")
 
-print("Logged in")
+message = "Hello %s %s, you are now Logged in" %(name, lastName)
+print(message)
