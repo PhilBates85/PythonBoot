0608bbff19a72bd9678f914abde0d6493992e5d1
philbates85 <phillipmbates85@gmail.com>
2018-11-25 13:12:19 -0800

    updated functions

--- TutorialPrograms/simplefunct0.py
+++ TutorialPrograms/simplefunct0.py
@@ -7,3 +7,10 @@ def force_mult(x):
     return 2.567 * x
 
 print(force_mult(2))
+
+
+
+def convertor(original_unit, coefficient=0.3048):
+    return original_unit * coefficient
+
+print(convertor(10))
