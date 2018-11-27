eb87919569c381ef208fb6701514e7cde7c7ea38
philbates85 <phillipmbates85@gmail.com>
2018-11-27 08:54:56 -0800

    Adding case sensitivity to the program

--- InteractiveDictionaryApp/app1.py
+++ InteractiveDictionaryApp/app1.py
@@ -1,9 +1,15 @@
 import json
-
+#creating a variable out of the json data file
 data = json.load(open("data.json"))
-
+#function to call definitions from the key value dictionary
+#adding a user friendly error message for mispelled words
+#the keys are all in lower case to prevent error force the words into lower case
 def translate(word):
-    return data[word]
+    word = word.lower()
+    if word in data:
+        return data[word]
+    else:
+        return "The word doesn't exsist. Please double check it"
 
 word = input("Enter word: ")
 
