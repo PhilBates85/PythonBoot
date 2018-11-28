8c7ba2a1b764d59302fbdc3688373b63a54d1675
philbates85 <phillipmbates85@gmail.com>
2018-11-28 14:16:42 -0600

    using a For loop option for multiple markers

--- WebMap/Map1.py
+++ WebMap/Map1.py
@@ -5,8 +5,9 @@ map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 #create a feature group
 fg = folium.FeatureGroup(name="My Map")
 fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
-
-
+#I can use a For loop to iterate multiple coordinates in a list
+for coordinates in [[39.4, -97.6],[36.4, -93.4]]:
+    fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a Marker", icon=folium.Icon(color='green')))
 #add feature
 map.add_child(fg)
 
